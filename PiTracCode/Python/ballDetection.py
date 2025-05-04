34a3bbb0b067c0c4a35400132df4fe7b4a223380
Sting Zhang <Stingzhang9000@gmail.com>
2025-05-03 18:26:23 -0700

    wrote ROI algorithm

--- PiTracCode/Python/ballDetection.py
+++ PiTracCode/Python/ballDetection.py
@@ -2,18 +2,7 @@ import cv2
 import numpy as np
 import json
 import os
-
-def convert_to_canny(image_path):
-    # Read the image in grayscale
-    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    
-    # Apply GaussianBlur to reduce noise and improve edge detection
-    blurred = cv2.GaussianBlur(img, (5, 5), 1.4)
-    
-    # Use Canny edge detection
-    edges = cv2.Canny(blurred, 100, 200)
-    
-    return edges
+from PiTracCode.Python.Convert_Canny import convert_to_canny
 
 image_path = "/home/vglalala/GCFive/Images/log_cam2_last_strobed_img.png"
 canny_image = convert_to_canny(image_path)
